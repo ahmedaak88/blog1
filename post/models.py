fdc1f7d7525f2e6580b92303f4550a931fd71d7a
ahmedaak88 <ahmedaak88@gmail.com>
2017-07-24 19:35:06 +0300

    we added methodes to the models

--- post/models.py
+++ post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -10,3 +11,9 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absoulute_url(self):
+		return reverse("post:detail", kwargs={"post_id": self.id})
+	def name_val(self):
+		return str(self.id) 
+		pass
