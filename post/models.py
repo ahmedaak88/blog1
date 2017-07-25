7f3635d5b81d1595f41ed4ae8c9b2ef8e19446fb
ahmedaak88 <ahmedaak88@gmail.com>
2017-07-25 18:47:05 +0300

    we created the delete update pages

--- post/models.py
+++ post/models.py
@@ -14,6 +14,12 @@ class Post(models.Model):
 
 	def get_absoulute_url(self):
 		return reverse("post:detail", kwargs={"post_id": self.id})
+	def update_url_post(self):
+		return reverse("post:update", kwargs={"post_id": self.id})
+	def delete_url(self):
+		return reverse("post:delete", kwargs={"post_id": self.id})
+	def create_url(self):
+		return redirect("post:create")
 	def croping(self):
 		return str(self.content[0:30])
 		pass
--- post/views.py
+++ post/views.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
+from django.contrib import messages
 
 def post_detail(request, post_id):
     obj = get_object_or_404(Post , id = post_id)
@@ -27,15 +28,33 @@ def post_list(request):
     }
     return render(request, 'post_list.html',context3)
 def post_create(request):
-    form = PostForm()
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request,"you are king of the world")
+        return redirect ("post:list")
     context = {
     "form": form,
     }
     return render(request, 'post_create.html',context)
 
     
-def post_update(request):
-    return render(request, 'post_update.html',{})
+def post_update(request, post_id):
+    post_obj = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None ,instance=post_obj)
+    if form.is_valid():
+        form.save()
+        messages.success(request,"how dare you ?")
+        return redirect ("post:list")
+    context = {
+    "form": form,
+    "post_obj": post_obj,
+    }
+    return render(request, 'post_update.html',context)
 
-def post_delete(request):
-    return render(request, 'post_delete.html',{})
+def post_delete(request, post_id):
+    post_obj = Post.objects.get(id=post_id)
+    post_obj.delete()
+    messages.warning(request, "dont you dare")
+    return redirect("post:list")
+    
